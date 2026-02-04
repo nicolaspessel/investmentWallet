8f5c1a5b54c8330bb953b36d3d63c9f2168cf500
nicolaspessel <nicolaspessel@gmail.com>
2026-02-03 23:19:06 -0300

    Creating the classes

---
+++ assets.py
@@ -0,0 +1,14 @@
+class Portifolio():
+    ...
+
+class Assets:
+    def __init__(self, code, name, div, amount, price):
+        self.code = code
+        self.name = name
+        self.div = div  
+        self.amount = amount
+        self.medium_price = price
+        self.curr_price = price      
+
+class Transaction():
+    ...
