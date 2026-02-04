89b9fb927aa1d20f90cdc7c4e1d2a6c31744985a
nicolaspessel <nicolaspessel@gmail.com>
2026-02-03 23:22:11 -0300

    Creating assets class methods

--- assets.py
+++ assets.py
@@ -8,7 +8,25 @@ class Assets:
         self.div = div  
         self.amount = amount
         self.medium_price = price
-        self.curr_price = price      
+        self.curr_price = price 
+
+    def calculate_invested_val(self):           # Here we don't need to pass amount, for example, because the instance
+        return self.amount * self.medium_price  # already have the self.amount when initialized, and so does the method.
+
+    def calculate_actual_val(self):
+        return self.amount * self.curr_price
+
+    def calculate_valorization(self):  # Calculation functions dont't update values, they calculate and return!
+        return self.calculate_actual_val() - self.calculate_invested_val()
+    
+    def update_price(self, new_price):  # Update functions don't return, they just update values!
+        self.curr_price = new_price
+    
+    def purchase(self, amount, price):
+        invested_val = self.calculate_invested_val()
+        invested_val += (amount * price)
+        self.amount += amount
+        self.medium_price = invested_val / self.amount       
 
 class Transaction():
     ...
