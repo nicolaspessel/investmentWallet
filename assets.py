e5772f2d0221f70e0d27d80d5ae6d1187e6dd4f1
nicolaspessel <nicolaspessel@gmail.com>
2026-02-12 19:24:58 -0300

    feat: class Assets finished for now.

--- assets.py
+++ assets.py
@@ -1,32 +1,38 @@
 class Portifolio():
     ...
 
+
+class Transaction():
+    ...
+        
+
 class Assets:
-    def __init__(self, code, name, div, amount, price):
+    def __init__(self, code:str, name:str, type:str, amount:int | 0, price:float | 0):
         self.code = code
         self.name = name
-        self.div = div  
+        self.type = type  
         self.amount = amount
         self.medium_price = price
         self.curr_price = price 
 
-    def calculate_invested_val(self):           # Here we don't need to pass amount, for example, because the instance
+    def _calculate_invested_val(self):           # Here we don't need to pass amount, for example, because the instance
         return self.amount * self.medium_price  # already have the self.amount when initialized, and so does the method.
 
-    def calculate_actual_val(self):
+    def _calculate_actual_val(self):
         return self.amount * self.curr_price
 
     def calculate_valorization(self):  # Calculation functions dont't update values, they calculate and return!
         return self.calculate_actual_val() - self.calculate_invested_val()
     
-    def update_price(self, new_price):  # Update functions don't return, they just update values!
+    def update_price(self, new_price:float):  # Update functions don't return, they just update values!
         self.curr_price = new_price
     
-    def purchase(self, amount, price):
-        invested_val = self.calculate_invested_val()
-        invested_val += (amount * price)
-        self.amount += amount
-        self.medium_price = invested_val / self.amount       
-
-class Transaction():
-    ...
+    def purchase(self, amount:int, price:float):
+        if self.amount == 0:  # First possible purchase
+            self.amount = amount
+            self.medium_price = price
+        else:        
+            invested_val = self.calculate_invested_val()
+            invested_val += (amount * price)
+            self.amount += amount
+            self.medium_price = invested_val / self.amount       
