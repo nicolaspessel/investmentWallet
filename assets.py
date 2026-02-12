0e1398fdf4ff4bcb2fee8ec7f7394c6c3249c508
nicolaspessel <nicolaspessel@gmail.com>
2026-02-12 19:40:09 -0300

    feat: initializing transaction class

--- assets.py
+++ assets.py
@@ -3,11 +3,12 @@ class Portifolio():
 
 
 class Transaction():
-    ...
+    def __init__(self) -> None:
+        pass
         
 
 class Assets:
-    def __init__(self, code:str, name:str, type:str, amount:int | 0, price:float | 0):
+    def __init__(self, code:str, name:str, type:str, amount:int | 0, price:float | 0) -> None:
         self.code = code
         self.name = name
         self.type = type  
@@ -15,16 +16,16 @@ class Assets:
         self.medium_price = price
         self.curr_price = price 
 
-    def _calculate_invested_val(self):           # Here we don't need to pass amount, for example, because the instance
-        return self.amount * self.medium_price  # already have the self.amount when initialized, and so does the method.
+    def _calculate_invested_val(self) -> float | int:  # Here we don't need to pass amount, for example, because the instance
+        return self.amount * self.medium_price         # already have the self.amount when initialized, and so does the method.
 
-    def _calculate_actual_val(self):
+    def _calculate_actual_val(self) -> float | int:
         return self.amount * self.curr_price
 
-    def calculate_valorization(self):  # Calculation functions dont't update values, they calculate and return!
+    def calculate_valorization(self) -> float | int:  # Calculation functions dont't update values, they calculate and return!
         return self.calculate_actual_val() - self.calculate_invested_val()
     
-    def update_price(self, new_price:float):  # Update functions don't return, they just update values!
+    def update_price(self, new_price:float) :  # Update functions don't return, they just update values!
         self.curr_price = new_price
     
     def purchase(self, amount:int, price:float):
